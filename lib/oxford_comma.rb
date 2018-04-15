3a444c839e3bbced21964d8a4bacca9d4522f568
Ji Yim <gsooyim@gmail.com>
2018-04-15 01:07:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,8 @@ def oxford_comma(array)
   if array.size <= 1
     array.join(", ")
   else 
+    last_word = array.pop
     new_string = array.join(", ")
-    last_word = new_string.pop
     new_string << "and #{last_word}"
   end
 end
