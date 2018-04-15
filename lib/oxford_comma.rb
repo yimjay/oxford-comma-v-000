acfe4334660d5fa6cb8d3dbd7de2e3d418e40e9b
Ji Yim <gsooyim@gmail.com>
2018-04-15 01:08:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   if array.size <= 1
     array.join(", ")
+  elsif array.size == 2
+    array.join(" and ")
   else 
     last_word = array.pop
     new_string = array.join(", ")
