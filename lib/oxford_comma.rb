9dda799de8af3c0042e7cdd7df43846af2819715
Ji Yim <gsooyim@gmail.com>
2018-04-15 01:20:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.size <= 1
+    array.join()
+  elsif array.size == 2
+    array.join(" and ")
+  else 
+    last_word = array.pop
+    new_string = array.join(", ")
+    new_string << ", and #{last_word}"
+  end
 end
